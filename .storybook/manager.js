7f7ad4bf66499b16fae128864bddb0adad1d50da
Mauricio Robayo <rfmajo@gmail.com>
2021-08-20 20:24:03 -0500

    chore(storybook): show zoom
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -4,7 +4,6 @@ import { create } from "@storybook/theming";
 addons.setConfig({
   showNav: false,
   toolbar: {
-    zoom: { hidden: true },
     eject: { hidden: true },
     fullscreen: { hidden: true },
   },
